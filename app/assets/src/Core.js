406e93ee61203220e32ac67c78d4feae7377eefa
KayanHendges <kayanbhendges@gmail.com>
2022-07-08 11:06:49 -0300

    feat: create the cep reponse html handler

--- app/assets/src/Core.js
+++ app/assets/src/Core.js
@@ -1,18 +1,32 @@
 async function getCepData(event){
   event.preventDefault();
 
-  var cepInput = document.getElementById("cep-input")
-  var cep = cepInput.value
+  const cepInput = document.getElementById("cep-input");
+  const cepValue = cepInput.value;
+
+  const cepAddress = document.getElementById("cep-address");
+  const cepCity = document.getElementById("cep-city");
+  const cepNumber = document.getElementById("cep-number");
 
   // implement validate cep
 
-  const cepData = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+  const cepData = await fetch(`https://viacep.com.br/ws/${cepValue}/json/`)
   .then(response => { return response.json() })
-  .catch(err => { return console.log(err) })
+  .catch(err => { return console.log(err) });
 
   if(!cepData){
   // implement erro handler e controll error span
-  }
+  };
+
+  const { 
+      logradouro, complemento,
+      bairro, localidade, uf,
+      cep 
+  } = cepData;
+
+  cepAddress.innerHTML = `${logradouro}${complemento? `, ${complemento}` : ''},`;
+  cepCity.innerHTML = `${bairro} - ${localidade}/${uf}.`;
+  cepNumber.innerHTML = `Cep: ${cep}`;
 
 }
 
