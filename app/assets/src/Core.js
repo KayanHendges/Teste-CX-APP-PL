91e5613c3174e17b485b791a492192d16ee35644
KayanHendges <kayanbhendges@gmail.com>
2022-07-08 10:22:42 -0300

    feat: create fetch cep data function

--- app/assets/src/Core.js
+++ app/assets/src/Core.js
@@ -1,8 +1,18 @@
 async function getCepData(event){
-
   event.preventDefault();
 
   var cepInput = document.getElementById("cep-input")
+  var cep = cepInput.value
+
+  // implement validate cep
+
+  const cepData = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+  .then(response => { return response.json() })
+  .catch(err => { return console.log(err) })
+
+  if(!cepData){
+  // implement erro handler e controll error span
+  }
 
 }
 
