a31721a10e13e4e2be04afc2f9066486544dfbea
KayanHendges <kayanbhendges@gmail.com>
2022-07-11 15:25:06 -0300

    update: cep header

--- app/assets/src/components/CepForm/html.js
+++ app/assets/src/components/CepForm/html.js
@@ -1,6 +1,6 @@
 const CepForm = `
 <form id="cep-form">
-    <h2 id="cep-header">Informações do Cep</h2>
+    <h2 id="cep-header">Informações do CEP</h2>
     <span id="cep-error"></span>
     <div id="input-container">
         <input type="text" id="cep-input" placeholder="insira o cep">
